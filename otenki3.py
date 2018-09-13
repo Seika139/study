895ff344bbb04a49b80bb227dd935018c76f9c11
Seika139 <tcoanzu@gmail.com>
2018-09-13 15:33:08 +0900

    studyファイルの整理をおこなった

--- otenki3.py
+++ otenki3.py
@@ -16,30 +16,31 @@ t = Twitter(auth=OAuth(ACCESS_TOKEN,
 
 r_num = randint(0,2,5)       #  0〜1 の整数を5個生成
 
-t.statuses.update(status="今日寒すぎ")
+t.statuses.update(status="外人ぽい人が自販機の前で「この中のどのコーヒーが甘いですか?」と聞かれたが、そんなこと聞かれても...") #投稿するツイート
 
 
-result1 = pywapi.get_weather_from_weather_com('JAXX0085') 
+
+result1 = pywapi.get_weather_from_weather_com('JAXX0085')
 h_tug = "#デレマス朝の天気予報"
 
 def idlst(key_num,c_max,c_min,cp, hum, w_s):
 	i_l = {0:
-		{ 
+		{
 		"name" : "udsuki",
 		"aisatsu" : "プロデューサーのみなさん、おはようございます！島村卯月です。今日の東京の天気をお伝えします。",
-		"tenki" : { 
-"sunny" : "今日の天気は晴れ。気持ちのいい一日になりそうです。", 
-"p_cloudy" : "今日は少し曇りますが、概ね晴れる予想です。", 
-"m_cloudy" : "今日は曇り空の広がる一日となりそうです。" , 
-"rainy" : "今日の天気は雨、", 
+		"tenki" : {
+"sunny" : "今日の天気は晴れ。気持ちのいい一日になりそうです。",
+"p_cloudy" : "今日は少し曇りますが、概ね晴れる予想です。",
+"m_cloudy" : "今日は曇り空の広がる一日となりそうです。" ,
+"rainy" : "今日の天気は雨、",
 "storm" : "今日は嵐が襲ってきます。プロデューサーさん気をつけてくださいね。",
 "am_rain" : "今日は午前中に雨が降る予報です。",
 "pm_rain" : "今日は午後に雨が降る予報です。" ,
 "shower":"今日はにわか雨が降る予報です。" ,
 "else":"今日は風の強い一日となるでしょう。"},
 "kion" : "最高気温は" +str(c_max) +"度、最低気温は" +str(c_min) +"度です。",
-"comment" : { 
-"cold" : 
+"comment" : {
+"cold" :
 { "tf" : False , "text" : "今日は最高気温が"+str(c_max)+ "度で、肌寒い一日となります。風邪には気をつけて今日もお仕事頑張りましょう。"},
 "hot" : { "tf" : False , "text" : "今日は最高気温が" + str(c_max)+ "度の暑い一日となりそうです。熱中症には気をつけてくださいね。"},
 "w_rain" : { "tf" : False , "text" : "降水確率は" + str(cp) + "%、傘を持っていったほうがいいかもしれませんね。"},
@@ -54,19 +55,19 @@ def idlst(key_num,c_max,c_min,cp, hum, w_s):
 1:
 { "name" : "rin" ,
 "aisatsu" : "みんなおはよう。渋谷凛です。今日の東京の天気を発表するよ。",
-"tenki" : { 
-"sunny" : "今日の天気は晴れていい天気だよ", 
-"p_cloudy" : "今日は大体晴れるって。", 
-"m_cloudy" : "今日はまあまあ曇るみたい。" , 
-"rainy" : "今日の天気は雨、", 
+"tenki" : {
+"sunny" : "今日の天気は晴れていい天気だよ",
+"p_cloudy" : "今日は大体晴れるって。",
+"m_cloudy" : "今日はまあまあ曇るみたい。" ,
+"rainy" : "今日の天気は雨、",
 "storm" : "今日は嵐がくるよ。ハナコが心配。",
 "am_rain":"今日は午前中に雨が降る予報です。",
 "pm_rain":"今日は午後に雨が降る予報です。",
 "shower":"今日はにわか雨が降るみたいだよ。",
 "else":"今日は風の強い一日となるでしょう。"},
 "kion" : "最高気温は" +str(c_max) +"度、最低気温は" +str(c_min) +"度。",
-"comment" : { 
-"cold" : 
+"comment" : {
+"cold" :
 { "tf" : False , "text" : "今日は最高気温が"+str(c_max)+ "度しかないから結構寒くなるよ。"},
 "hot" : { "tf" : False , "text" : "今日は最高気温が" + str(c_max)+ "を越えるから暑いって。"},
 "w_rain" : { "tf" : False , "text" : "降水確率は" + str(cp) + "%、傘を持っていったほうがいいかもよ。"},
@@ -98,12 +99,12 @@ def tweet(g_w,r_n):
 	c_max = g_w[2]
 	c_min = g_w[3]
 	cp = g_w[4]
-	
-	
+
+
 	i_l = idlst(r_num[0],c_max,c_min,cp,g_w[5],g_w[6])
 	rain_s = False
 	wind_s = False
-	
+
 	#tenki文の決定
 	if "Sunny" in g_w[1]:
 		key = "sunny"
@@ -128,11 +129,11 @@ def tweet(g_w,r_n):
 		rain_s = True
 	else:
 		key = "else"
-	
+
 	if type(g_w[6]) == int:
 		if int(g_w[6]) >= 20:
 			wind_s = True
-	
+
 	#comment文の決定
 	if wind_s == True and rain_s == True:
 		key2 = "wind_and_rain"
@@ -147,7 +148,7 @@ def tweet(g_w,r_n):
 			key2 = "nothing"
 	elif wind_s == True:
 		key2 = "strong_wind"
-		
+
 	else:
 		key2_s = []
 		if int(c_max) <= 10:
@@ -159,14 +160,14 @@ def tweet(g_w,r_n):
 		if int(g_w[5]) <= 30:
 			i_l["comment"]["dry"]["tf"] = True
 			key2_s.append("dry")
-		
+
 		if key2_s == []:
 			key2 = "nothing"
 		else:
 			r_com = randint(0,len(key2_s))
 			key2 = key2_s[r_com]
-			
-			
+
+
 	print("今日は", date , "日です。")
 	print(i_l["aisatsu"])
 	print(i_l["tenki"][key])
